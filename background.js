e03fff08d0f2be308f8e8f4d3349e3f0631a2202
Ludumo Toni <ludumo.toni@outlook.com>
2021-08-25 17:47:29 +0200

    day 3 run & jump

--- background.js
+++ background.js
@@ -1,6 +1,18 @@
 class Background {
     draw() {
-      /*   console.log('this is background drawing'); */
-        image(game.backgroundImages[0].src, 0, 0, 600, 600)
+        console.log('this is background drawing'); 
+        console.log(width, height); 
+
+        game.backgroundImages.forEach(function(img) {
+            img.x -= img.speed;
+            image(img.src, img.x, 0, width, height);  
+            // we add second image
+            image(img.src, img.x + width, 0, width, height); 
+        //if the image moves out of screen then needs to be reset
+            if(img.x <= - width) {
+                 img.x = 0; 
+            }
+        })
+
     }
 }
--- game.js
+++ game.js
@@ -5,7 +5,14 @@ class Game {
 
     preloadGame() {
         this.backgroundImages = [
-            {src:loadImage('background pics/1.png')}
+            {src:loadImage('background pics/1.png'), x: 0, speed: 0},
+            {src:loadImage('background pics/2.png'), x: 0, speed: 1},
+            {src:loadImage('background pics/3.png'), x: 0, speed: 2},
+            {src:loadImage('background pics/4.png'), x: 0, speed: 3},
+            {src:loadImage('background pics/5.png'), x: 0, speed: 4},
+            {src:loadImage('background pics/6.png'), x: 0, speed: 5},
+            {src:loadImage('background pics/7.png'), x: 0, speed: 6},
+            {src:loadImage('background pics/8.png'), x: 0, speed: 7},
             // load image function
         ]
     }
